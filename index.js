3f6abf3b7c9b1452d32ab9bda8c38b0082c2a132
md anas <anas2542wy@gmail.com>
2025-12-07 01:09:27 +0600

    added my listing

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/myads',async( req, res)=>{
+      const {email} = req.query
+      const query = {email: email}
+      const result = await petServices.find(query).toArray()
+      res.send(result)
+      
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
